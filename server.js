652ad0d05e75d78e33ba6d920b1c615b09b83f0b
JosephGlz <joseph220199@gmail.com>
2023-06-01 18:37:39 -0600

    server configuration

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const hbs = require('hbs')
+require('dotenv').config()
+const db = require('./database/controller')
+const sesion = require('./extras/sessions')
 
+/**
+ * SERVER CONFIGURATION
+ */
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, './views'))
+hbs.registerPartials(path.join(__dirname, './views/partials'))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json({ limit: '100mb' }))
+app.use(sesion)
+require('./extras/helpers')
 
-const port = process.env.PORT || 3001
+app.use(express.static(path.join(__dirname, './assets')))
+app.use(require('./routes/index'))
+
+const port = process.env.PORT || 3000
+
+/**
+ * SERVER INITIALIZATION
+ */
 
 app.listen(port, () => {
-    console.log(`[SERVER]: Starting server ${port}`)
+    console.log(`[SERVER]: Starting server...`)
 })
 .on('listening', () => {
-    console.log(`[SERVER]: Server started ${port}.`)
+    console.log(`[SERVER]: Server started on ${port}`)
+    console.log(`[SERVER]: Live on ${process.env.APP_URL}`)
 })
 .on('error', (err) => {
     console.log(`[SERVER]: Error trying to start the server on ${port}.`)
